3f34d308fd7bd20cda4c7f1f7da97a68bccc967e
callbritton <cla.inbox@gmail.com>
2021-04-29 15:49:50 -0400

    Add photo to seed file
    Co-authored-by: Dani Coleman <danicoleman00@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+photo1 = Photo.create!(title: "Beautiful mountains", description: "Look at dem mountains", genre: "landscape", url: 'https://cdn.pixabay.com/photo/2016/05/24/16/48/mountains-1412683__340.png')
