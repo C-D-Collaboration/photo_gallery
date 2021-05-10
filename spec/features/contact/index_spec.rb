d48c55182f777fbfa5ba3eb9076820074ac4e51e
Dani Coleman <danicoleman00@gmail.com>
2021-05-10 17:33:33 -0600

    Update path to use path helper

--- spec/features/contact/index_spec.rb
+++ spec/features/contact/index_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Contact Page' do
   describe 'visit /contact' do
     it "can see a contact page with contact information and a form" do
-      visit "/contact"
+      visit contact_path
+      
       expect(page).to have_content("Contact")
       expect(page).to have_content("Have a question?")
       expect(page).to have_content("Please don't hesitate to reach out for any reason or just fill out the form and hit 'submit'.")
