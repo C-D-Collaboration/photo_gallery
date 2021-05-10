44033be991518b9e99eaa66f86b19373e6fb392e
Dani Coleman <danicoleman00@gmail.com>
2021-05-10 16:59:16 -0600

    Update expectations for contact page

--- spec/features/contact/index_spec.rb
+++ spec/features/contact/index_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'Contact Page' do
   describe 'visit /contact' do
     it "can see a contact page with contact information and a form" do
       visit "/contact"
-      expect(page).to have_content('Contact')
+      expect(page).to have_content("Contact")
       expect(page).to have_content("Have a question?")
-      expect(page).to have_content("Please don't hesitate to reach out for any reason!")
+      expect(page).to have_content("Please don't hesitate to reach out for any reason or just fill out the form and hit 'submit'.")
       expect(page).to have_content("email: danicoleman00@gmail.com")
     end
   end
