b1897e1ec34be8d4d0e1a538071af72935cbe6de
Dani Coleman <danicoleman00@gmail.com>
2021-05-13 16:01:21 -0600

    Add create action, create a new contact object from contact poro
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,3 +1,24 @@
 class ContactController < ApplicationController
   def index;end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    #currently getting an undefined method 'save' for #<Contact:0x00007fbc01bfa3c8> error
+    if @contact.save
+      ContactMailer.with(contact: @contact).new_contact_email.deliver_later
+
+      flash[:success] = "Thank you for your message! We'll get back to you soon!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Your message had some errors. Please check the form and resubmit."
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.permit(:first_name, :last_name, :email, :subject, :message)
+  end
 end
