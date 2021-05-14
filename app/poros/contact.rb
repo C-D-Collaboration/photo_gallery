1863ff95519d4fe5318ce337b676383b279dcf40
callbritton <cla.inbox@gmail.com>
2021-05-14 16:01:24 -0400

    Update full name and add attr_readers
    Co-authored-by: Dani Coleman <danicoleman00@gmail.com>

--- app/poros/contact.rb
+++ app/poros/contact.rb
@@ -1,12 +1,18 @@
 class Contact
-  #do we need an attr reader for all of these attributes?
+  attr_reader :first_name,
+              :last_name,
+              :email,
+              :subject,
+              :message,
+              :full_name
+              
   def initialize(info)
-    @full_name = full_name
     @first_name = info[:first_name]# :first_name
     @last_name = info[:last_name]# :last_name
     @email = info[:email]# :email
     @subject = info[:subject]# :subject
     @message = info[:message]# :message
+    @full_name = full_name
   end
 
   def full_name
