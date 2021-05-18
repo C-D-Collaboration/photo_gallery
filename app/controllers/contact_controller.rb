fba581a4cd4168f350f2149d94f6749251bd338d
Dani Coleman <danicoleman00@gmail.com>
2021-05-18 09:54:01 -0600

    Implemented create logic from action mailer docs. WIP
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,13 +2,26 @@ class ContactController < ApplicationController
   def index;end
 
   def create
-    contact = Contact.new(contact_params)
-    # looks like it is sending but the flash message does not show up and it is not redirecting
-    ContactMailer.new_contact_email(contact).deliver_now
+    @contact = Contact.new(contact_params)
+    respond_to do |format|
+      if @contact
+        # Tell the UserMailer to send a welcome email after save
+        ContactMailer.with(contact: @contact.as_json).new_contact_email.deliver_later
 
-    flash[:success] = "Thank you for your message! We'll get back to you soon!"
-    redirect_to root_path
+        format.html { redirect_to(@contact, notice: 'Contact was successfully created.') }
+        format.json { render json: @contact, status: :created, location: @contact }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
   end
+  #   contact = Contact.new(contact_params)
+  #   ContactMailer.new_contact_email(contact).deliver_later
+  #
+  #   flash[:success] = "Thank you for your message! We'll get back to you soon!"
+  #   redirect_to root_path
+  # end
 
   private
 
