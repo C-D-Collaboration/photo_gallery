c07c3de74f064130bddb9fda99a4b0de976da789
Dani Coleman <danicoleman00@gmail.com>
2021-05-18 09:53:40 -0600

    Update email beind displayed on contact page
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- spec/features/contact/index_spec.rb
+++ spec/features/contact/index_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe 'Contact Page' do
   describe 'visit /contact' do
     it "can see a contact page with contact information and a form" do
       visit contact_path
-      
+
       expect(page).to have_content("Contact")
       expect(page).to have_content("Have a question?")
       expect(page).to have_content("Please don't hesitate to reach out for any reason or just fill out the form and hit 'submit'.")
-      expect(page).to have_content("email: danicoleman00@gmail.com")
+      expect(page).to have_content("email: danicolemanphotography@gmail.com")
     end
   end
 end
