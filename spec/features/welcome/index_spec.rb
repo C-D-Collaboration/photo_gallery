0505449a1c6a087e58da40487c06fad977464ffb
Callbritton <63476564+Callbritton@users.noreply.github.com>
2021-05-18 12:03:22 -0400

    Revert "Mailer"

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Welcome Page' do
   describe 'visit /' do
     it "can see a title and description" do
-      visit root_path
-      expect(page).to have_content('Dani Coleman')
-      expect(page).to have_content("Photography")
+      visit "/"
+      expect(page).to have_content('Dani Coleman - Through the Lens of Light')
+      expect(page).to have_content("Here is how I see the world and its infinite vastness!")
     end
   end
 end
